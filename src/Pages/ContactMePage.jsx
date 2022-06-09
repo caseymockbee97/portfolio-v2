6473aa7e03896aa1f1438e748129b7bc908ebdff
Casey Mockbee <caseymockbee97@gmail.com>
2022-06-09 15:31:07 -0400

    Fixed contact me accessability. Updated icons.

--- src/Pages/ContactMePage.jsx
+++ src/Pages/ContactMePage.jsx
@@ -8,37 +8,42 @@ export default function ContactMePage() {
 				<div className="contact-container">
 					<h1 className="contact-container-header">Contact Me</h1>
 					<ul className="contact-container-list">
-						<a
-							target="_blank"
-							rel="noreferrer"
-							className="contact-container-list-link"
-							href="https://github.com/caseymockbee97"
-						>
-							<li className="contact-container-list-link-container">
-								GitHub <AiFillGithub />
-							</li>
-						</a>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							className="contact-container-list-link"
-							href="https://www.linkedin.com/in/casey-mockbee/"
-						>
-							<li className="contact-container-list-link-container">
-								LinkedIn <AiFillLinkedin />
-							</li>
-						</a>
-						<a
-							target="_blank"
-							rel="noreferrer"
-							className="contact-container-list-link"
-							href="mailto: caseymockbee97@gmail.com"
-						>
-							<li className="contact-container-list-link-container">
-								<span>Email </span>
-								<AiOutlineSend />
-							</li>
-						</a>
+						<li className="contact-container-list-item">
+							<a
+								target="_blank"
+								rel="noreferrer"
+								className="contact-container-list"
+								href="https://github.com/caseymockbee97"
+							>
+								<div className="contact-container-list-link-container">
+									GitHub <AiFillGithub />
+								</div>
+							</a>
+						</li>
+						<li className="contact-container-list-item">
+							<a
+								target="_blank"
+								rel="noreferrer"
+								className="contact-container-list"
+								href="https://www.linkedin.com/in/casey-mockbee/"
+							>
+								<div className="contact-container-list-link-container">
+									LinkedIn <AiFillLinkedin />
+								</div>
+							</a>
+						</li>
+						<li className="contact-container-list-item">
+							<a
+								target="_blank"
+								rel="noreferrer"
+								className="contact-container-list"
+								href="mailto: caseymockbee97@gmail.com"
+							>
+								<div className="contact-container-list-link-container">
+									Email <AiOutlineSend />
+								</div>
+							</a>
+						</li>
 					</ul>
 				</div>
 			</div>
