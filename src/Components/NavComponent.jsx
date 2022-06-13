c95e4ed9ea79e503d68b3c67e6bf01289fe5cc5d
Casey Mockbee <caseymockbee97@gmail.com>
2022-06-13 17:51:48 -0400

    Added profile image

--- src/Components/NavComponent.jsx
+++ src/Components/NavComponent.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { projects } from "../Assets/ProjectInfo";
+import ProfileImage from "../Assets/Images/ProfileImage.png";
 
 export default function NavComponent() {
 	return (
 		<nav>
+			<img
+				aria-hidden
+				src={ProfileImage}
+				className="nav-image"
+				alt="Cartoon Profile"
+			/>
 			<a href="#home">Home</a>
 			<a href="#skills">Skills</a>
 			<a href="#projects">Projects</a>
