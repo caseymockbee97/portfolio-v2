61cf0d4240e741eaa830423b73874738f9e230bd
Casey Mockbee <caseymockbee97@gmail.com>
2022-08-30 16:25:57 -0400

    Added visual indicator for tab navigation

--- src/Components/LinkItemComponent.jsx
+++ src/Components/LinkItemComponent.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import { useState } from "react";
 
 export default function LinkItemComponent(props) {
 	let { loaded, title, index, icon, link } = props;
 
+	const [isActive, setIsActive] = useState(false);
+
+	const handleFocus = () => setIsActive(true);
+	const handleBlur = () => setIsActive(false);
+
 	return (
 		<li
-			className={`list-link ${loaded ? "loaded" : "not-loaded"}`}
+			className={`list-link ${loaded ? "loaded" : "not-loaded"} ${
+				isActive ? "active" : ""
+			}`}
 			style={{ transitionDelay: index * 0.05 + "s" }}
 		>
-			<a target="_blank" rel="noreferrer" className="list-link-a" href={link}>
+			<a
+				target="_blank"
+				rel="noreferrer"
+				className="list-link-a"
+				href={link}
+				onFocus={handleFocus}
+				onBlur={handleBlur}
+			>
 				<div className="list-link-container">
 					{title} {icon}
 				</div>
